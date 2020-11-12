a0338311fb933600440e2f60f1e33a42b2f3b37e
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2020-11-12 09:15:01 -0600

    added line 2 code

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         //write your code here
         System.out.println("Hello");
-        // Line 1: Today is Thursday
+        // line 1: Today is Thursday
+        // line 2: Tomorrow is Friday
     }
 }
